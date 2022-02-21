ba1204ba3a2ee9e539cc863c6a6e65a92546290d
ArtiFICR <bowabla@gmail.com>
2022-02-21 14:13:36 -0600

    Added form to Pay page and stylized pay form

--- crypto-wallet/src/pages/pay/Pay.jsx
+++ crypto-wallet/src/pages/pay/Pay.jsx
@@ -1,7 +1,9 @@
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import { Box, TextField } from "@mui/material";
 
 import "../../styles/gridStyles.css";
+import "./pay.css";
 
 const Pay = () => {
   return (
@@ -10,7 +12,48 @@ const Pay = () => {
       <div className="contactContainer">
         <Header headerText="Pay" />
         <div className="contentContainer">
-          
+          <div className="payContainer">
+            <Box
+              sx={{
+                margin: "auto",
+                height: "36rem",
+                width: "45rem",
+                backgroundColor: "#656a6b",
+                borderRadius: "5px",
+                border: "1px solid #aaaaaa",
+                color: "#bfbfbf",
+              }}
+            >
+              <p className="payComponentHeader">Pay</p>
+              <div className="amountToSend">amtToSendInUSD</div>
+              <div className="payInputContainer">
+                <nav className="selectAsset">
+                  <p className="selectAssetLabel">Pay with:</p>
+                  <TextField 
+                    fullWidth
+                    size="small"
+                    className="selectAssetInput"
+                  />
+                </nav>
+                <nav className="walletToPay">
+                  <p className="toWalletLabel">To:</p> 
+                  <TextField 
+                    fullWidth
+                    size="small"
+                    className="toWalletInput"
+                  />
+                </nav>
+                <nav className="toMessage">
+                  <p className="messageLabel">Message: </p>
+                  <TextField 
+                    fullWidth 
+                    size="small"
+                    className="messageInput"
+                  />
+                </nav>
+              </div>
+            </Box>
+          </div>
         </div>
       </div>
     </div>
